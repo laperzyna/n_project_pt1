d2d3e6443f4f219c6cd3d11c2f1e2e9728204140
Maleke Hanson <mchanson3@vlab00.cs.usfca.edu>
2022-10-09 20:36:01 -0700

    Yes

--- client.c
+++ client.c
@@ -14,13 +14,31 @@
 
 #define BUF_SIZE 500
 
+
 int main(argc char *argv[]){
+
+	struct addrinfo hintsUDP;
+	struct addrinfo *resultUDP, *rpUDP;
+	int getaddrinfoValueUDP;
+
+	memset(&hintsUDP, 0, sizeof(hintsUDP));
+	
 
 
 	int udpSocket;
 	int optionValue = 1;
 	struct sockaddr_in udpSockAddr;
 
+	udpSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP
+	if(udpSocket == 0){
+		if(setsockopt(udpSocket,SOL_SOCKET, SO_REUSEADDR, &optionValue, sizeof(optionValue))== 0){
+			if(bind(udpSocket, ))
+		}	
+	}
+
+	
+
+	
 
 
 

f388eb2cf8bd44790bc8ec5551d66e3e48309f87
Maleke Hanson <mchanson3@vlab00.cs.usfca.edu>
2022-10-09 19:55:34 -0700

    First Commit

---
+++ client.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+
+#define BUF_SIZE 500
+
+int main(argc char *argv[]){
+
+
+	int udpSocket;
+	int optionValue = 1;
+	struct sockaddr_in udpSockAddr;
+
+
+
+
+	
+}
